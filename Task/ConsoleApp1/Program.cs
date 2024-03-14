7a234840a77390387a8cdc5712f35b1aa2d9acba
gurbanli11 <orkhan.80@bk.ru>
2024-03-15 00:22:14 +0400

    create Task.sln file

---
+++ Task/ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TASK 1
+            //int num = 45687;
+            //int max = 0;
+            //int count = 0;
+            //while (num>0)
+            //{
+            //    count = num % 10;
+            //    if (max < count )
+            //    {
+            //        max= count;
+            //    }
+            //    num= num / 10;
+            //}
+            //Console.WriteLine(max);
+
+
+            //TASK 2
+            //int num = 471;
+            //int max = 0; 
+            //while (num!=0)
+            //{
+            //    max = num % 10;
+            //    num /= 10;
+            //}
+            //if (num%10>max)
+            //{
+            //    num /= 10;
+            //}
+            //Console.WriteLine();
+
+
+            //TASK 3
+            //int[] nums = { 5, 11, -5723, 13, 421, 87, 5724, 505 };
+            //int min = nums[0];
+            //int max = nums[0];
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (min > nums[i])
+            //    {
+            //        min = nums[i];
+            //    }
+            //    if (max < nums[i])
+            //    {
+            //        max = nums[i];
+            //    }
+            //}
+            //Console.WriteLine("En kicik eded " + min);
+            //Console.WriteLine("En boyuk eded " +max);
+            //Console.WriteLine("Cemi:" + (min+max));
+
+
+
+            //TASK 4
+            //int[] nums = { 2, 5,9, 27, 12, 33, 39, 99, 30, 41 };
+            //int num = 0;
+            //int count = 0;
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (nums[i] % 2 != 0 && nums[i] % 3 == 0)
+            //    {
+            //    num = nums[i];
+
+            //    Console.WriteLine(num);
+
+            //    }
+
+            //}
+        }
+    }
+}
